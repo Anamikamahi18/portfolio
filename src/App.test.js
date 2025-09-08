7b7250120af3596f7d2fff612ac681250bb941a5
Anamikamahi18 <anamikamahi18@gmail.com>
2025-09-09 04:26:57 +0530

    Updated App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders portfolio home', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // Check for the "View My Work" button, which is reliably present
+  expect(await screen.findByText(/View My Work/i)).toBeInTheDocument();
 });
